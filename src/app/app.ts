0f73651e474bad729216911d96ffac6d2667c3a7
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2025-12-09 23:10:39 -0500

    Add scrolling and box setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
+
 import { routes } from './app.routes'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
-  template: `
-    
-
-    <router-outlet></router-outlet>
-  `
+  imports: [
+    Home,
+    About,
+    Contact,
+    Skills,
+    Projects
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Portfolio');
+
 }
