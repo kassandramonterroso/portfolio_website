254faf02370ef8fa788146410936499b2c978c3f
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2025-12-10 23:25:35 -0500

    project grids

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './projects.css',
 })
 export class Projects {
+  
+  selectedProject: any = null;
+
+  projects: {[key: number] : any} = {
+    1: {
+      title: 'Real-Time Sentiment Tracker',
+      description: 'Live social media sentiment analysis using VADER and BERT NLP, displayed with a Streamlit dashboard.',
+      tech: ['Python', 'BERT', 'VADER', 'Pandas'],
+      /* image: 'assets/project1_full.png */
+    }
+  };
+
+  openProject(id: number){
+    this.selectedProject = this.projects[id];
+  }
+
+  closeModal(){
+    this.selectedProject = null;
+  }
 
 }
