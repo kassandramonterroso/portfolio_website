f7ff1ab144f97da4c85116e787621c730c4a3e84
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2025-12-11 23:43:04 -0500

    contact page completed

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+
+  submitted=false;
+
+  submitForm(){
+    this.submitted=true;
+  }
+
+  closeModal(){
+    this.submitted=false;
+  }
 
 }
