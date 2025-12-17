f88d39c372a07345927967e89392a62b00a80cde
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2025-12-16 19:35:12 -0500

    skills page and animation

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Host, HostListener} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
 export class Skills {
+
+  scrollY= 0;
+
+  getOffset(i:number) {
+    return Math.sin((this.scrollY/150) + i) * 20;
+  }
+
+  skills = [
+    {
+      category: 'Frontend',
+      items: ['Angular', 'Typescript', 'HTML', 'CSS']
+    },
+    {
+      category: 'Backend',
+      items: ['Python', 'Java', 'SpringBoot', 'REST APIs']
+    },
+    {
+      category: 'Databases',
+      items: ['PostgreSQL', 'SQL']
+    },
+    {
+      category: 'Cloud & DevOps',
+      items: ['Docker', 'Git', 'CI/CD', 'AWS']
+    }
+  ];
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrollY = window.scrollY;
+  }
 
 }
